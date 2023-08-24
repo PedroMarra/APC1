987a8e6e7bbec88da41d8681de1073c84ad53c5b
PedroMarra1 <25529749-PedroMarra1@users.noreply.replit.com>
2023-08-24 00:46:15 +0000

    arquivos da aula 1

---
+++ aulas/aula01/meuprogama.c
@@ -0,0 +1,15 @@
+/* 
+  Autor: Pedro Braga
+  Data: 23/08/23
+
+  progama que exibe ola pessoal!
+  */
+#include <stdio.h>
+#include <math.h>
+
+// funcao principal
+int main() {
+  printf("ola pessoal!\n");
+  printf("a raiz quadrade de 2 é%f\n"), sqrt(2));
+  return 0;
+}
