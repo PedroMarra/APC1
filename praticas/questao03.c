8e2d0f1381d51bf61fa6c0312c5d543b84039ea1
PedroMarra <142457845+PedroMarra@users.noreply.github.com>
2023-08-31 00:49:56 +0000

    codigo da aula 02 e da pratica 01

---
+++ praticas/questao03.c
@@ -0,0 +1,18 @@
+/*
+Faça um programa em C que calcule o perímetro de uma pizza (P = 2 x PI x r). Considere o raio um número inteiro e a constante PI igual a 3,1416.
+*/
+
+#include <stdio.h>
+int main() {
+
+  float pi = 3.1416;
+  float raio = 6;
+
+  float perimetro = 3.0f* pi * raio;
+
+  printf("O perimetro é %f\n*", perimetro);
+  
+  return 0;
+  
+}
+
