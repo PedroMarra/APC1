2be3520da7507d5d4b24b1d472d631f368de1696
PedroMarra <142457845+PedroMarra@users.noreply.github.com>
2023-09-21 00:42:04 +0000

    código da aula06

--- praticas/questao01.c
+++ praticas/questao01.c
@@ -7,10 +7,15 @@ Faça um programa em C que calcule a média final a partir da fórmula (0,4 x A1
 int main() {
 float nota_a1 = 5.5f;
 float nota_a2 = 4.0f;
+printf(" Digite sua nota A1: ");
+scanf("%f", &nota_a1);
+
+  printf(" Digite sua nota A2: ");
+  scanf("%f", &nota_a2);
 
 float media = 0.4 * nota_a1 + 0.6 * nota_a2;
 
-printf("A media é %f\n", media);
+printf("A media é %3.2f\n", media);
 
 return 0;
 }
